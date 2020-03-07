4b2e61ba52a6f996a935b9edbd21b6780b0a3c43
sajidmahmud69 <54687706+sajidmahmud69@users.noreply.github.com>
2020-03-06 22:46:10 -0500

    New files added

---
+++ Project 1/MyColor.java
@@ -0,0 +1,27 @@
+/*package  sample;
+
+import java.awt.Color;
+
+public enum MyColor {
+    BLACK, BLUE, CYAN, DARK_GRAY, GRAY,
+    GREEN, MAGENTA, ORANGE, LIGHT_GRAY,
+    RED, WHITE, YELLOW, PINK
+
+
+    private int r, g, b;
+    private int rgb;
+
+    // Constructor
+
+    MyColor(int rgb){ this.rgb = rgb; }
+    MyColor (int r, int g, int b){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public int getRGB() {
+        return 0xFF000000;
+    }
+}
+ */
---
+++ Project 1/module-info.java
@@ -0,0 +1,6 @@
+module Assignment1 {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.desktop;
+    opens sample;
+}
