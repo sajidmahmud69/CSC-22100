c9e1dec3675f79603692c64eff88e4a7323029c5
sajidmahmud69 <54687706+sajidmahmud69@users.noreply.github.com>
2020-03-30 22:45:58 -0400

    Delete MyPoint.java

--- Project2/MyPoint.java
+++
@@ -1,7 +0,0 @@
-public interface MyPoint {
-    public void setXCoordinates (double inX);
-    public void setYCoordinates (double inY);
-    public double [] getXYCoordinates ();
-    public void moveTo (double x, double y);
-    public double distanceTo(double x, double y);
-}
